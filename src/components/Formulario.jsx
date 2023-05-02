8c69c1c280d590edf9ef1aee3670f29baa823883
nehemias <nehemiasrivero1@gmail.com>
2023-05-02 16:54:41 -0400

    Changes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,13 +7,53 @@ const Formulario = () => {
 <span className="text-indigo-600 font-bold">Administralos</span>
 
 </p>
-<form className="bg-white shadow-md rounded-lg py-10 px-5">
-  <div>
-    <label className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
-    <input type="text"
+<form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+  <div className="mb-5">
+    <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+    <input 
+    id="mascota"
+    type="text"
     placeholder="Nombre de la Mascota"
     className="border-2 w-full mt-2 placeholder-gray-400 rounded" />
   </div>
+  <div  className="mb-5">
+    <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+    <input 
+    id="propietario"
+    type="text"
+    placeholder="Nombre del Propietario"
+    className="border-2 w-full mt-2 placeholder-gray-400 rounded" />
+  </div>
+
+  <div  className="mb-5">
+    <label htmlFor="email" className="block text-gray-700 uppercase font-bold">Email</label>
+    <input 
+    id="email"
+    type="email"
+    placeholder="Email contacto Propietario"
+    className="border-2 w-full mt-2 placeholder-gray-400 rounded" />
+  </div>
+  <div  className="mb-5">
+    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Alta</label>
+    <input 
+    id="alta"
+    type="date"
+    className="border-2 w-full mt-2 placeholder-gray-400 rounded" />
+  </div>
+
+  <div  className="mb-5">
+    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">Sintomas</label>
+    <textarea  id="sintomas" 
+    className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+    placeholder="Describe los Sintomas "
+    ></textarea>
+    
+  </div>
+
+<input type="submit"
+className="bg-indigo-600 w-full p-3 tet-white uppercase font-bold hover:bg-indigo-700 cursor-pointer"
+value="Agregar Paciente"
+/>
 </form>
 
     </div>
