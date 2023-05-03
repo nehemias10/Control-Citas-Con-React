26740ae241cd37e569c69661bfc99b611e4fdebf
nehemias <nehemiasrivero1@gmail.com>
2023-05-03 10:24:56 -0400

    cambios2

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -3,7 +3,17 @@
 const ListadoPacientes = () => {
   return (
     <div className="w-1/2 lg:w-3/5">
-      listado de pacientes
+
+      <h2 className="font-black text-3xl text-center">listado de pacientes</h2>
+<p className="text-xl mt-5 mt-10 text-center">Administra tus {""}</p>
+<span className="text-indigo-600 font-bold">Pacientes y CItas</span>
+    
+    <div className="mt-5 bg-white">
+<p>Nombre:
+  <span>Hook</span>
+</p>
+
+    </div>
     </div>
   )
 }
