2a369237971e92d7975cdd8f049c202aab857623
nehemias <nehemiasrivero1@gmail.com>
2023-05-03 18:34:29 -0400

    hook y eventos

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,19 +1,19 @@
+import Paciente from "./Paciente"
 
 
 const ListadoPacientes = () => {
   return (
-    <div className="w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll mt-6">
 
       <h2 className="font-black text-3xl text-center">listado de pacientes</h2>
-<p className="text-xl mt-5 mt-10 text-center">Administra tus {""}</p>
-<span className="text-indigo-600 font-bold">Pacientes y CItas</span>
-    
-    <div className="mt-5 bg-white">
-<p>Nombre:
-  <span>Hook</span>
-</p>
-
-    </div>
+      <p className="text-xl mt-5 mt-10 text-center">Administra tus {""}
+      <span className="text-indigo-600 font-bold">Pacientes y CItas</span>
+      </p>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+      <Paciente/>
+     
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,34 @@
+
+
+const Paciente = () => {
+    return (
+        <div className="mx-5 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
+                <span className="font-normal normal-case">Hook</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
+                <span className="font-normal normal-case">Nehemias</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
+                <span className="font-normal normal-case">nehemias@gmail.com</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""}
+                <span className="font-normal normal-case">10 de mayo de 2023</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""}
+                <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint architecto magnam consequuntur quibusdam dolores cumque? Eum quod a ducimus quibusdam nam, commodi error vel quam quos expedita doloribus? Itaque, corrupti?</span>
+            </p>
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Paciente
